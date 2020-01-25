d89bc8d32ce977dd64df24834671ccd04e3ac0fa
ohheyitsdave <david.lackovic@me.com>
2020-01-25 11:13:00 +0100

    :lipstick: styling homepage :lipstick: styling menu

--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -33,11 +33,53 @@
     <meta property="og:image" content=""/>
 </svelte:head>
 
-<section>
+<section class="md:block hidden">
     <div id="particles-js"></div>
 
-    <div class="bg-gray-200 md:overflow-hidden">
+    <div class="bg-gray-200">
+<!--        <div class="px-4 py-10">-->
+<!--            <div class="relative md:max-w-2xl md:mx-auto text-center">-->
+<!--                <div class="p-2">-->
+<!--                    <div class="inline-flex items-center bg-white leading-none text-pink-600 rounded-full p-2 shadow text-sm">-->
+<!--                        <span class="inline-flex bg-pink-600 text-white rounded-full h-6 px-3 justify-center items-center text-">Donate</span>-->
+<!--                        <a href="{$locale}/donate"-->
+<!--                           target="_blank" rel="noopener, noreferrer, nofollow">-->
+<!--                            <span class="inline-flex px-2">Help us keep the network alive by donating a small amount 💓</span>-->
+<!--                        </a>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
 
+
+        <svg class="fill-current bg-gray-200 text-white pt-10"
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 1440 320">
+            <path fill-opacity="1"
+                  d="M0,64L120,85.3C240,107,480,149,720,149.3C960,149,1200,107,1320,85.3L1440,64L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+            ></path>
+        </svg>
+        </div>
+    <div class="w-3/4 mx-auto bg-white shadow-lg relative z-20 hidden md:block"
+         style="margin-top: -20rem; border-radius: 15px;">
+
+        <div class="flex" style="height: 600px;">
+            <iframe scrolling="no"
+                    src="https://maps.sensor.community/?selection=PM25&nooverlay=true#2/28.3/-1.1"
+                    style="width: 97%; height: 95%; margin: auto; overflow: hidden;"
+                    title="sensor.community particulate matter map"></iframe>
+        </div>
+    </div>
+    <div class="px-4 py-16">
+        <div class="relative md:max-w-2xl md:mx-auto text-center">
+            <h1 class="font-bold text-gray-700 text-xl md:text-4xl leading-tight mb-6">
+                {$_('index.h1')}</h1>
+        </div>
+    </div>
+</section>
+
+<section class="xs:block hidden">
+    <div class="bg-gray-200">
         <div class="px-4 py-10">
             <div class="relative md:max-w-2xl md:mx-auto text-center">
                 <div class="p-2">
@@ -49,13 +91,6 @@
                         </a>
                     </div>
                 </div>
-
-                <!--            <h1 class="font-bold text-gray-700 text-xl md:text-4xl leading-tight mb-6">-->
-
-                <!--                {$_('index.h1')}-->
-                <!--            </h1>-->
-                <!--			<div class="hidden md:block h-40 w-40 rounded-full bg-blue-800 absolute right-0 bottom-0 -mb-64 -mr-48"></div>-->
-                <!--			<div class="hidden md:block h-5 w-5 rounded-full bg-yellow-500 absolute top-0 right-0 -mr-40 mt-32"></div>-->
             </div>
         </div>
 
@@ -68,21 +103,13 @@
         </svg>
 
     </div>
-    <div class="w-3/4 mx-auto bg-white shadow-lg relative z-20 hidden md:block"
-         style="margin-top: -23rem; border-radius: 15px;">
-        <!--	<div class="h-20 w-20 rounded-full bg-yellow-500 absolute top-0 left-0 -ml-10 -mt-10"-->
-        <!--			style="z-index: -1;">-->
-        <!--	</div>-->
-
-        <!--	<div class="h-5 w-5 rounded-full bg-blue-500 absolute top-0 left-0 -ml-32 mt-12"-->
-        <!--			style="z-index: -1;">-->
-        <!--	</div>-->
-
-        <div class="flex" style="height: 600px;">
-            <iframe scrolling="no"
-                    src="https://maps.sensor.community/?selection=PM25&nooverlay=true#2/28.3/-1.1"
-                    style="width: 97%; height: 95%; margin: auto; overflow: hidden;"
-                    title="sensor.community particulare matter map"></iframe>
+    <div class="px-4 ">
+        <div class="-mt-10 max-w-4xl mx-auto bg-white shadow-lg relative z-20"
+             style="margin-top: -8rem; height:600px">
+            <div class="bg-white rounded-t-lg border-b border-gray-100"></div>
+            <iframe scrolling="no" src="https://maps.sensor.community/?selection=PM25"
+                    style="width: 100%; height: 100%; border: none; overflow: hidden;"
+                    title="sensor.community particulate matter map"></iframe>
         </div>
     </div>
     <div class="px-4 py-16">
@@ -93,15 +120,7 @@
     </div>
 </section>
 
-<div class="px-4 md:hidden">
-    <div class="-mt-10 max-w-4xl mx-auto bg-white shadow-lg relative z-20"
-         style="border-radius: 20px;">
-        <div class="h-10 bg-white rounded-t-lg border-b border-gray-100"></div>
-        <iframe scrolling="no" src="https://maps.sensor.community/?selection=PM25"
-                style="width: 100%; border: none; overflow: hidden;"
-                title="sensor.community particulare matter map"></iframe>
-    </div>
-</div>
+
 
 <section class="bg-gray-200 py-8">
     <div class="container mx-auto px-4 text-center">
