b34df8154aa78fb0d16beaa174333ab701c7653b
ohheyitsdave <david.lackovic@me.com>
2020-08-24 13:06:39 +0200

    :fire: removed campaigns from home

--- src/components/Campaigns.svelte
+++ src/components/Campaigns.svelte
@@ -54,38 +54,6 @@
                         </div>
                     </div>
                 </a>
-                <a href="https://sustainabledevelopment.un.org/?menu=1300" target="_blank">
-                    <div class="mx-auto flex flex-wrap items-center justify-center text-gray-700 ">
-                        <div class="flex-shrink-0 m-6 relative overflow-hidden rounded-lg max-w-xs shadow-md border bg-gray-200 h-64 w-128">
-                            <div class="relative px-6 pb-6 mt-6 hover:text-teal-700">
-                                <div class="flex justify-between">
-                                    <span class="text-4xl flex">#GlobalGoals</span>
-                                </div>
-                                <span class="opacity-75 text-3xl -mb-1">United Nations SDG</span>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-                <a href="https://www.earthday.org/earth-day-2020/" target="_blank">
-                    <div class="flex-shrink-0 m-6 relative overflow-hidden rounded-lg max-w-xs shadow-md border bg-gray-200 h-64 w-128">
-                        <div class="relative px-6 pb-6 mt-6 hover:text-teal-700">
-                            <div class="flex justify-between">
-                                <span class="text-4xl flex">️#EC2020</span>
-                            </div>
-                            <span class="opacity-75 text-3xl -mb-1">Wilson Center & EPA</span>
-                        </div>
-                    </div>
-                </a>
-                <a href="https://cleanairday.eu/" target="_blank">
-                    <div class="flex-shrink-0 m-6 relative overflow-hidden rounded-lg max-w-xs shadow-md border bg-gray-200 h-64 w-128">
-                        <div class="relative px-6 pb-6 mt-6 hover:text-teal-700">
-                            <div class="flex justify-between">
-                                <span class="text-4xl flex">️#CAD2020</span>
-                            </div>
-                            <span class="opacity-75 text-3xl -mb-1">Clean Air Day </span>
-                        </div>
-                    </div>
-                </a>
                 <a href="{lang}/campaign/sensor2school">
                     <div class="flex-shrink-0 m-6 relative overflow-hidden rounded-lg max-w-xs shadow-md border bg-gray-200 h-64 w-128">
                         <div class="relative px-6 pb-6 mt-6 hover:text-teal-700">
